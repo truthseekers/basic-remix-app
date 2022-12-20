f72f3190c19ca93e59c1f20a2c05a654b0a40618
John Curry <fastpenguin91@gmail.com>
2022-12-20 10:11:14 -0800

    whatever

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -15,7 +15,6 @@ export const loader = async (props: LoaderArgs) => {
             }
         }
     }`;
-  console.log("fuuuuck is this?");
 
   const posts = await getClient()
     .fetch(query)
@@ -38,23 +37,7 @@ export default function Index() {
       <article>
         <h2>Blog Posts</h2>
         <h3>Welcome to my blog posts page!</h3>
-        <div>
-          {/* @ts-ignore */}
-          {posts.map((post, index) => (
-            <a href={`/${post.slug.current}`} key={index}>
-              <span key={index}>
-                <img
-                  style={{ width: "25%" }}
-                  src={post.mainImage.asset.url}
-                  alt=""
-                />
-                <span>
-                  <h2>{post.title}</h2>
-                </span>
-              </span>
-            </a>
-          ))}
-        </div>
+        <div></div>
       </article>
 
       <ul>
